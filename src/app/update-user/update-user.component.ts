a9dc73130c2076dbb3646cb65cdff1d8933bbbfa
unknown <danielevy29@gmail.com>
2023-06-26 19:29:11 +0200

    acolade_manquante

--- src/app/update-user/update-user.component.ts
+++ src/app/update-user/update-user.component.ts
@@ -9,30 +9,33 @@ import { UserServiceService } from 'src/services/userService/user-service.servic
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent {
-    idUser: any;
-    userForm: FormGroup;
+  idUser: any;
+  userForm: FormGroup;
 
-    constructor(private route: ActivatedRoute, 
-      private Router: Router,
-      @Inject(FormBuilder) private formBuilder: FormBuilder,
-      private us :UserServiceService){
-        this.userForm = this.formBuilder.group({
-            nom: new FormControl(""),
-            prenom: new FormControl(""),
-            role: new FormControl(""),
-            email: new FormControl(""),
-        })
-        this.idUser = "";
-    }
-    ngOnInit(): void {
-      this.idUser = this.route.snapshot.paramMap.get("id");
-      this.us.getUserById(this.idUser).subscribe((userdata : any) => {
-        this.userForm = this.formBuilder.group({
-          nom: new FormControl(userdata.nom),
-          prenom: new FormControl(userdata.lastname),
-          role: new FormControl(userdata.email),
-          email: new FormControl(userdata.role)
-      })
-    }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private userService: UserServiceService
+  ) {
+    this.userForm = this.formBuilder.group({
+      nom: new FormControl(''),
+      prenom: new FormControl(''),
+      role: new FormControl(''),
+      email: new FormControl('')
+    });
+    this.idUser = '';
+  }
 
+  ngOnInit(): void {
+    this.idUser = this.route.snapshot.paramMap.get('id');
+    this.userService.getUserById(this.idUser).subscribe((userdata: any) => {
+      this.userForm = this.formBuilder.group({
+        nom: new FormControl(userdata.nom),
+        prenom: new FormControl(userdata.prenom),
+        role: new FormControl(userdata.role),
+        email: new FormControl(userdata.email)
+      });
+    });
+  }
 }
